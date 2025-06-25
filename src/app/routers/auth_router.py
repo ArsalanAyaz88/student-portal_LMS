453448671badf9692870cd3697141f290190d41d
ArsalanAyaz88 <arsalanayaz821@gmail.com>
2025-06-25 16:59:43 +0500

    removed double routes

--- src/app/routers/auth_router.py
+++ src/app/routers/auth_router.py
@@ -3,5 +3,5 @@
 from fastapi import APIRouter
 from src.app.controllers import auth_controller
 
-router = APIRouter(prefix="/api/auth", tags=["Authentication"])
-router.include_router(auth_controller.router) 
+router = APIRouter(tags=["Authentication"])
+router.include_router(auth_controller.router)
