e305e8d0600fbf731e6182f3f29faf3b4d4220b1
ArsalanAyaz88 <arsalanayaz821@gmail.com>
2025-06-30 22:16:57 +0500

    allow ccredentials sets

--- src/app/main.py
+++ src/app/main.py
@@ -48,7 +48,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Allows all origins
-    allow_credentials=True, # Allows credentials (e.g., Authorization headers)
+    allow_credentials=False, # Allows credentials (e.g., Authorization headers)
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
 )
