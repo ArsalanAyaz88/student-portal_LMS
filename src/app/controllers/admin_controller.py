16e85b08e3a8054a15f7f55231cfaa87267e917f
ArsalanAyaz88 <arsalanayaz821@gmail.com>
2025-07-17 21:57:37 +0500

    course update

--- src/app/controllers/admin_controller.py
+++ src/app/controllers/admin_controller.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException, Query
+from fastapi import APIRouter, Depends, HTTPException, Query, Request
 from sqlmodel import Session, select
 from sqlalchemy import func
 import uuid
