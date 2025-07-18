96a4d37d83b6e65029565074f7cdcb7654d5028a
ArsalanAyaz88 <arsalanayaz821@gmail.com>
2025-07-18 11:27:31 +0500

    img url

--- src/app/main.py
+++ src/app/main.py
@@ -76,6 +76,7 @@ app.include_router(course_router.router, prefix="/api/courses", tags=["Courses"]
 
 app.include_router(enrollment_router.router, prefix="/api/enrollments", tags=["Enrollments"])
 app.include_router(admin_router.router, prefix="/api/admin", tags=["Admin"])
+app.include_router(admin_router.router, prefix="/api", tags=["Admin"])
 app.include_router(sa_router.router, prefix="/api/student/assignments")
 app.include_router(sq_router.router, prefix="/api/student")
 app.include_router(
