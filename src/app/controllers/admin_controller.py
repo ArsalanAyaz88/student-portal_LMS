f420d19613c4e71ad5b18c736f9c56b8452d79f8
Ahsan <ahs462agk@gmail.com>
2025-08-07 14:14:08 +0500

    solve schema error

--- src/app/controllers/admin_controller.py
+++ src/app/controllers/admin_controller.py
@@ -49,7 +49,7 @@ from src.app.schemas.course import (
 )
 from src.app.schemas.enrollment_application_schema import EnrollmentApplicationRead, EnrollmentApplicationUpdate
 from src.app.schemas.notification import NotificationRead, AdminNotificationRead
-from src.app.schemas.quiz import QuizCreate, QuizReadWithDetails, QuizRead, QuizCreateForVideo
+from src.app.schemas.quiz import QuizCreate, QuizReadWithDetails, QuizRead
 from src.app.schemas.user import UserRead
 from src.app.schemas.video import VideoAdminRead, VideoCreate, VideoRead, VideoUpdate
 
